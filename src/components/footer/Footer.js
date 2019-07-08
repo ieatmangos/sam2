5ae851f00940cdd44cc4ffc57ac934b789137e5f
GabrielLoubier <gabrielloubier1@gmail.com>
2019-07-08 16:14:41 +0800

    another

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,15 @@ export class Footer extends Component {
     render() {
         return (
             <div className='footer'>
-                <h1>footer</h1>
+
+                <div className='footer-right'>
+                    <h1> Company </h1>
+                    <i class="fa fa-facebook-f"></i>
+                    <i class="fa fa-instagram"></i>
+                    <i class="fa fa-twitter"></i>
+
+                </div>
+                <div className='mango'><p>Website by Eat Mangos</p></div>
             </div>
         )
     }
